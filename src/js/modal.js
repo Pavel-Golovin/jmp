c90c17cd22b149d9335b457f8994daf76848f88b
Pavel <Golovin.Pavel@outlook.com>
2020-10-10 22:40:13 +0300

    Запрограммировал интерактив по модалке Обратная связь

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,20 +1,50 @@
 'use strict';
 
 const feedback = document.querySelector('.main__feedback');
-const feedbackButton = document.querySelector('.contacts-list__chat');
-const closeFeedbackButton = document.querySelector('.modal__close-feedback');
+const feedbackButtons = document.querySelectorAll('.contacts-list__chat');
+const closeFeedbackButton = document.querySelector('.modal__close-btn--feedback');
+const overlay = document.querySelector('.overlay');
+
 
 const openFeedbackPopup = () => {
   feedback.classList.add('main__feedback--open');
-  closeFeedbackButton.classList.add('modal__close-feedback--toggle');
+  overlay.classList.add('overlay--active');
+  closeFeedbackButton.classList.remove('modal__close-btn--hidden');
 }
 
 const closeFeedbackPopup = () => {
   feedback.classList.remove('main__feedback--open');
-  closeFeedbackButton.classList.remove('modal__close-feedback--toggle');
+  overlay.classList.remove('overlay--active');
+  closeFeedbackButton.classList.add('modal__close-btn--hidden');
 }
 
-feedbackButton.addEventListener('click', () => {
-  openFeedbackPopup();
-  closeFeedbackButton.addEventListener('click', closeFeedbackPopup);
-});
+const closeFeebackByEsc = (evt) => {
+  if (evt.key === 'Escape') {
+    closeFeedbackPopup();
+  }
+};
+
+feedbackButtons.forEach((elem) => {
+  elem.addEventListener('click', () => {
+    openFeedbackPopup();
+    overlay.addEventListener('click', closeFeedbackPopup);
+    closeFeedbackButton.addEventListener('click', closeFeedbackPopup);
+    document.addEventListener('keydown', closeFeebackByEsc);
+  });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
