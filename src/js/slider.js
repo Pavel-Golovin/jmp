89168a6732b5ab36bd269d4fb1bfde9a93ae43b7
Pavel <Golovin.Pavel@outlook.com>
2020-10-13 09:31:32 +0300

    Рефакторинг src/js/slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,72 +1,39 @@
 'use strict';
 
-const brandsContainer = document.querySelector('.section__content--brands');
-const devicesContainer = document.querySelector('.section__content--devices');
-const servicesContainer = document.querySelector('.section__content--services');
+const brandsContainer = document.querySelector(`.section__content--brands`);
+const devicesContainer = document.querySelector(`.section__content--devices`);
+const servicesContainer = document.querySelector(`.section__content--services`);
 let brandsSwiper;
 let devicesSwiper;
 let servicesSwiper;
 let isSwiper = false;
+
+const parametersOfSlider = {
+  slidesPerView: 1.2,
+  spaceBetween: 16,
+  pagination: {
+    el: `.swiper-pagination`,
+    clickable: true,
+  },
+  breakpoints: {
+    440: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    660: {
+      slidesPerView: 3
+    }
+  }
+}
 
 const initSwiper = () => {
   if (document.documentElement.clientWidth < 768) {
     if (isSwiper) {
       return false
     }
-
-    brandsSwiper = new Swiper(brandsContainer, {
-      slidesPerView: 1.2,
-      spaceBetween: 16,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      breakpoints: {
-        440: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        660: {
-          slidesPerView: 3
-        }
-      }
-    });
-
-    devicesSwiper = new Swiper(devicesContainer, {
-      slidesPerView: 1.2,
-      spaceBetween: 16,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      breakpoints: {
-        440: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        660: {
-          slidesPerView: 3
-        }
-      }
-    });
-
-    servicesSwiper = new Swiper(servicesContainer, {
-      slidesPerView: 1.2,
-      spaceBetween: 16,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      breakpoints: {
-        440: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        660: {
-          slidesPerView: 3
-        }
-      }
-    });
+    brandsSwiper = new Swiper(brandsContainer, parametersOfSlider);
+    devicesSwiper = new Swiper(devicesContainer, parametersOfSlider);
+    servicesSwiper = new Swiper(servicesContainer, parametersOfSlider);
     isSwiper = true;
   } else {
     if (!isSwiper) {
