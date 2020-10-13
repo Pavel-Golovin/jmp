ffdb73e1660144962901717bbafd8238039e4a3d
Pavel <Golovin.Pavel@outlook.com>
2020-10-13 07:18:44 +0300

    Рефакторинг: окончательная версия прайс-листа, плюс заголовок таблицы

--- src/js/show-content.js
+++ src/js/show-content.js
@@ -8,8 +8,6 @@ showAndHideToggles.forEach((elem, i) => {
   elem.addEventListener('click', function() {
     this.innerText = (this.innerText === 'Показать всё') ? 'Скрыть' : 'Показать всё';
     this.classList.toggle('toggle-btn--reversed');
-    console.log(sectionContents[0]);
-    console.log(sectionContents[1]);
     sectionContents[i].classList.toggle('section__content--all');
   });
 });
