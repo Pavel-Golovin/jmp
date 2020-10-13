3f8b9b1b9d3923bbcce246a9c4bb3a02dd161a2a
Pavel <Golovin.Pavel@outlook.com>
2020-10-13 11:26:07 +0300

    Рефакторинг - кнопки скрытия/показа контента

--- src/js/show-content.js
+++ src/js/show-content.js
@@ -1,13 +1,17 @@
 'use strict';
 
-const showAndHideToggles = document.querySelectorAll('.section__show-all');
-const sectionContents = document.querySelectorAll('.section__content');
-const [toggleBrands, toggleDevices] = showAndHideToggles;
+const showAndHideToggles = document.querySelectorAll('.toggle-btn');
+const articleContainer = document.querySelector('.article__text');
+const brandsContainer  = document.querySelector('.section__content--brands');
+const devicesContainer  = document.querySelector('.section__content--devices');
+const containers = [articleContainer, brandsContainer, devicesContainer];
 
 showAndHideToggles.forEach((elem, i) => {
-  elem.addEventListener('click', function() {
-    this.innerText = (this.innerText === 'Показать всё') ? 'Скрыть' : 'Показать всё';
+  elem.addEventListener('click', function(evt) {
+    let elementTarget = evt.target;
+    let buttonText = (elementTarget.classList.contains('article__read-more')) ? `Читать далее` : `Показать всё`;
+    this.innerText = (this.innerText === buttonText) ? 'Скрыть' : buttonText;
     this.classList.toggle('toggle-btn--reversed');
-    sectionContents[i].classList.toggle('section__content--all');
+    containers[i].classList.toggle('show-all');
   });
 });
